1f1c2b6c7a7e17f2d67f26bbb498825389344e5e
peymanfazeli <peymanfazeli.work@gmail.com>
2025-03-31 13:37:31 +0330

    add menubar via config file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Menu from './components/Menu';
 import Header from './components/Header';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="App d-flex flex-column justify-content-center align-items-center">
+			<Menu />
 			<header className="App-header">
 				<Header />
 			</header>
---
+++ src/config/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+    menuItems: {
+        home: {
+            name: 'Home',
+            URL: '/',
+        },
+        github: {
+            name: 'GitHub',
+            URL: 'https://github.com/peymanfazeli'
+        },
+        linkedin: {
+            name: 'LinkedIn',
+            URL: 'https://www.linkedin.com/in/peymanfazeli/'
+        },
+        medium: {
+            name: 'Medium',
+            URL: 'https://medium.com/@peymanfazeli',
+        },
+        virgool: {
+            name: 'Virgool',
+            URL: 'https://virgool.io/@Peymanfazeli',
+        },
+    }
+};
