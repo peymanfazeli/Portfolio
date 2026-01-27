56dfd13991c7c8ffb396028e63dca380340f33ca
Peyman Fazeli <peymanfazeli.work@gmail.com>
2026-01-27 16:14:00 +0330

    fix(ContactForm): redirect page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -62,7 +62,7 @@ const ContactForm = () => {
         <FormContainer>
             <FormSection id="appointmentForm">
                 <h2 class="mt-2 mb-5">LETS TALK ABOUT YOU</h2>
-                <form class="appointment-form" netlify method="POST" name="contact" data-netlify-redirect="./">
+                <form class="appointment-form" netlify method="POST" name="contact">
                     <input type="hidden" name="form-name" value="contact" />
 
                     <FormGroup>
